5813286f7237a60237afbefb8cad8f7278b625e3
arashi <arashiwalker@gmail.com>
2018-10-19 10:40:34 -0700

    added buttons and buttons functions in html and js

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+$("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+  });
+
+});
