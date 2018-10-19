a1fd3e1718b594dc978469023424b7fbbaed8f2a
arashi <arashiwalker@gmail.com>
2018-10-19 14:06:47 -0700

    working function for Language matcher

--- js/scripts.js
+++ js/scripts.js
@@ -35,14 +35,60 @@ $("button#green").click(function() {
     $(".C-showing").hide()
     $(".ruby-showing").hide()
   });
-
 });
-// $(".clickable").click(function() {
-//     $(".walrus-showing").fadeToggle();
-//     $(".walrus-hidden").fadeToggle();
-//   });
-//
-//   $(".clickable2").click(function() {
-//     $(".cat-showing").fadeToggle();
-//     $(".cat-hidden").fadeToggle();
-//   });
+
+var nameInput;
+var signInput;
+var ageInput;
+
+function choose(){
+  if (ageInput > 20 && ageInput) {
+    $("#match").show();
+    $("#name").text(nameInput);
+    $("#unmatch").hide();
+    if(signInput === "front end" && ageInput <= 35) {
+      $("#code1").show();
+      $("#code2").hide();
+      $("#code3").hide();
+      $("#code4").hide();
+      }
+    else if(signInput === "back end" && ageInput <= 35) {
+      $("#code2").show();
+      $("#code1").hide();
+      $("#code3").hide();
+      $("#code4").hide();
+    }
+    else if(signInput === "Mobile" && ageInput <= 35) {
+      $("#code3").show();
+      $("#code1").hide();
+      $("#code2").hide();
+      $("#code4").hide();
+    }
+    else {
+      $("#code4").show();
+      $("#code1").hide();
+      $("#code2").hide();
+      $("#code3").hide();
+    }
+  }
+  else if (ageInput < 20 && ageInput) {
+    $("#unmatch").show();
+    $("#name1").text(nameInput);
+    $("#match").hide();
+    $("#code4").hide();
+    $("#code1").hide();
+    $("#code2").hide();
+    $("#code3").hide();
+  }
+}
+
+$(document).ready(function() {
+$("#detailsform").submit(function(event) {
+  event.preventDefault();
+  nameInput = $("input#addName").val();
+  signInput = $("select#addSign").val();
+  ageInput = parseInt($("input#addAge").val());
+  event.preventDefault();
+  choose(nameInput, signInput, ageInput);
+  });
+});
